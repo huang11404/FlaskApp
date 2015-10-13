f9b8df62063d296bf387824bed8b548b96740fad
jiyu <huang11404@gmail.com>
2015-10-13 11:25:49 +0800

    no print

--- __init__.py
+++ __init__.py
@@ -21,7 +21,7 @@ def index():
 	taobao=[dict(time=row[0], m1=row[1], m2=row[2],m3=row[3],m4=row[4],m5=row[5],m6=row[6],m7=row[7],m8=row[8],m9=row[9], \
 										 m10=row[10],m11=row[11],m12=row[12],m13=row[13],m14=row[14],m15=row[15],m16=row[16],m17=row[17], \
 										 m18=row[18]) for row in cur.fetchall()]
-	print taobao
+	#print taobao
 	g.db.close()
 
 	#for chart
@@ -48,7 +48,7 @@ def daily_interest_rate():
 	taobao=[dict(time=row[0], m1=row[1], m2=row[2],m3=row[3],m4=row[4],m5=row[5],m6=row[6],m7=row[7],m8=row[8],m9=row[9], \
 										 m10=row[10],m11=row[11],m12=row[12],m13=row[13],m14=row[14],m15=row[15],m16=row[16],m17=row[17], \
 										 m18=row[18]) for row in cur.fetchall()]
-	print taobao
+	#print taobao
 	g.db.close()
 
 	#for chart
